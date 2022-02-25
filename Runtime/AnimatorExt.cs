49f8f8fc23ae265e62af31230b1955f6e7d0c1da
ComradeVanti <kingvanti650@gmail.com>
2022-02-25 16:12:12 +0100

    Add extension for starting and waiting for animations

---
+++ Runtime/AnimatorExt.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace ComradeVanti.WaitForAnim
+{
+
+    public static class AnimatorExt
+    {
+
+        /// <summary>
+        ///     Plays an animation and returns a yield-instruction that finishes when
+        ///     the animation is done
+        /// </summary>
+        /// <param name="animator">The animator to play on</param>
+        /// <param name="animationName">The name of the animation</param>
+        /// <param name="layer">The animation-layer</param>
+        /// <returns>The yield-instruction</returns>
+        public static WaitForAnimationToFinish PlayAndWait(this Animator animator, string animationName, int layer = 0)
+        {
+            animator.Play(animationName, layer);
+            return new WaitForAnimationToFinish(animator, layer, animationName);
+        }
+
+    }
+
+}
