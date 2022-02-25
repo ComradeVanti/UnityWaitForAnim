4917d5780fc75eed45d43645df6c6fec3a1ed439
ComradeVanti <kingvanti650@gmail.com>
2022-02-25 16:05:07 +0100

    Add logic to wait for specific animation

---
+++ Runtime/WaitForAnimationToFinish.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace ComradeVanti.WaitForAnim
+{
+
+    /// <summary>
+    ///     Waits for an animation to finish
+    /// </summary>
+    public class WaitForAnimationToFinish : CustomYieldInstruction
+    {
+
+        private readonly string animationName;
+
+        private readonly Animator animator;
+        private readonly int layerIndex;
+
+
+        private AnimatorStateInfo StateInfo => animator.GetCurrentAnimatorStateInfo(layerIndex);
+
+        private bool CorrectAnimationIsPlaying => StateInfo.IsName(animationName);
+
+        private bool AnimationIsDone => StateInfo.normalizedTime >= 1;
+
+        public override bool keepWaiting => CorrectAnimationIsPlaying && !AnimationIsDone;
+
+
+        /// <summary>
+        ///     Creates a new yield-instruction
+        /// </summary>
+        /// <param name="animator">The animator to track</param>
+        /// <param name="layerIndex">The layer the animation is playing on</param>
+        /// <param name="animationName">The name of the animation</param>
+        public WaitForAnimationToFinish(Animator animator, int layerIndex, string animationName)
+        {
+            this.animator = animator;
+            this.layerIndex = layerIndex;
+            this.animationName = animationName;
+        }
+
+    }
+
+}
